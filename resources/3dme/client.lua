91e4ae10d9c18015778b3276aaa418ff55930433
reancodes <33007389+reancodes@users.noreply.github.com>
2019-11-06 02:58:14 +0100

    Udgivelse

---
+++ resources/3dme/client.lua
@@ -0,0 +1,106 @@
+-- Settings
+local color = { r = 255, g = 255, b = 255, alpha = 255 } -- Color of the text 
+local font = 100 -- Font of the text
+local time = 7000 -- Duration of the display of the text : 1000ms = 1sec
+local background = {
+    enable = false,
+    color = { r = 0, g = 0, b = 0, alpha = 255 },
+}
+local chatMessage = false
+local dropShadow = false
+
+-- Don't touch
+local nbrDisplaying = 1
+
+RegisterCommand('me', function(source, args)
+    local text = '' -- edit here if you want to change the language : EN: the person / FR: la personne
+    for i = 1,#args do
+        text = text .. ' ' .. args[i]
+    end
+    text = text .. ' '
+    TriggerServerEvent('3dme:shareDisplay', text)
+end)
+
+RegisterNetEvent('3dme:triggerDisplay')
+AddEventHandler('3dme:triggerDisplay', function(text, source)
+    local offset = 0.8 + (nbrDisplaying*0.14)
+    Display(GetPlayerFromServerId(source), text, offset)
+end)
+
+function Display(mePlayer, text, offset)
+    local displaying = true
+
+    -- Chat message
+    if chatMessage then
+        local coordsMe = GetEntityCoords(GetPlayerPed(mePlayer), false)
+        local coords = GetEntityCoords(PlayerPedId(), false)
+        local dist = Vdist2(coordsMe, coords)
+        if dist < 2500 then
+            TriggerEvent('chat:addMessage', {
+                color = { color.r, color.g, color.b },
+                multiline = false,
+                args = { text}
+            })
+        end
+    end
+
+    Citizen.CreateThread(function()
+        Wait(time)
+        displaying = false
+    end)
+    Citizen.CreateThread(function()
+        nbrDisplaying = nbrDisplaying + 1
+        print(nbrDisplaying)
+        while displaying do
+            Wait(0)
+            local coordsMe = GetEntityCoords(GetPlayerPed(mePlayer), false)
+            local coords = GetEntityCoords(PlayerPedId(), false)
+            local dist = Vdist2(coordsMe, coords)
+            if dist < 2500 then
+                DrawText3D(coordsMe['x'], coordsMe['y'], coordsMe['z'] -0.7 +offset, text)
+            end
+        end
+        nbrDisplaying = nbrDisplaying - 1
+    end)
+end
+
+function DrawText3D(x,y,z, text)
+    local onScreen,_x,_y = World3dToScreen2d(x,y,z)
+    local px,py,pz = table.unpack(GetGameplayCamCoord())
+    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+ 
+    local scale = ((1/dist)*2)*(1/GetGameplayCamFov())*100
+
+    if onScreen then
+
+        -- Formalize the text
+        SetTextColour(color.r, color.g, color.b, color.alpha)
+        SetTextScale(0.35, 0.35)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x, _y)
+            local factor = (string.len(text)) / 370
+        DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 20,20,20,150)
+        if dropShadow then
+            SetTextDropshadow(10, 100, 100, 100, 255)
+        end
+
+        -- Calculate width and height
+        BeginTextCommandWidth("STRING")
+        AddTextComponentString(text)
+        local height = GetTextScaleHeight(0.55*scale, font)
+        local width = EndTextCommandGetWidth(font)
+
+        -- Diplay the text
+        --SetTextEntry("STRING")
+        --AddTextComponentString(text)
+        --EndTextCommandDisplayText(_x, _y)
+
+        if background.enable then
+            DrawRect(_x, _y+scale/45, width, height, background.color.r, background.color.g, background.color.b , background.color.alpha)
+        end
+    end
+end
